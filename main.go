ec4e50cb0999614d0139dca26c4f5eb7c3371659
MaQueen <67810971+kotobuki562@users.noreply.github.com>
2022-06-12 08:56:59 +0900

    💄 usersテーブルの作成

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"todo_app/config"
+	"todo_app/app/models"
 )
 
 func main() {
+	/*
 	fmt.Println(config.Config.Port)
 	fmt.Println(config.Config.DbName)
 	fmt.Println(config.Config.SQLDriver)
 	fmt.Println(config.Config.LogFile)
 	log.Println("test")
+	*/
+
+	fmt.Println(models.Db)
+
 }
