ae146ec93b33317556a547bb1b9c4c6651108963
MaQueen <67810971+kotobuki562@users.noreply.github.com>
2022-06-12 10:57:53 +0900

    📝 userIdの紐づくtodos取得

--- main.go
+++ main.go
@@ -18,8 +18,8 @@ func main() {
 
 
 	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@example.com"
+	// u.Name = "test2"
+	// u.Email = "test2@example.com"
 	// u.Password = "testtest"
 	// fmt.Println(u)
 	// u.CreateUser()
@@ -40,17 +40,25 @@ func main() {
 	fmt.Println(u)
 	*/
 
-	user, _ := models.GetUser(2)
-	user.CreateTodo("Second Todo")
+	user, _ := models.GetUser(3)
+	user.CreateTodo("Third Todo")
 
-	todos, _ := models.GetTodos()
-	for _, v := range todos {
-		fmt.Println(v)
-	}
+
+
+	// todos, _ := models.GetTodos()
+	// for _, v := range todos {
+	// 	fmt.Println(v)
+	// }
 
 
 	// t, _ := models.GetTodo(1)
 	// fmt.Println(t)
+
+	user2, _ := models.GetUser(3)
+	todos, _ := user2.GetTodosByUser()
+	for _, v := range todos {
+		fmt.Println(v)
+	}
 
 
 	
