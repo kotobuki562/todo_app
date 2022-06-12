17395dcafa4d78820ee5bcd9f3c56c789a6fcdb9
MaQueen <67810971+kotobuki562@users.noreply.github.com>
2022-06-12 09:53:03 +0900

    📝 todoのテーブル作成

--- app/models/base.go
+++ app/models/base.go
@@ -19,6 +19,7 @@ var err error
 
 const (
 	tableNameUser = "users"
+	tableNameTodo = "todos"
 )
 
 func init() {
@@ -36,7 +37,15 @@ func init() {
 		created_at DATETIME)`,tableNameUser)
 
 		Db.Exec(cmdU)
-}
+
+		cmdT := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			content TEXT,
+			user_id INTEGER,
+			created_at DATETIME)`,tableNameTodo)
+
+			Db.Exec(cmdT)
+		}
 
 // uuidを作成する
 func createUUID() (uuidobj uuid.UUID) {
