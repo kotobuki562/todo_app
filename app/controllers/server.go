964b9a3803cd389492f0c97e207b633bf8b9f8e7
MaQueen <67810971+kotobuki562@users.noreply.github.com>
2022-07-08 20:53:54 +0900

    📝 UI読み込みのためのbootstrapとjQueryを取り込み

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -6,6 +6,11 @@ import (
 )
 
 func StartMainServer() error {
+	// viewsディレクトリにあるjsとcssを読み込む
+	files := http.FileServer(http.Dir(config.Config.Static))
+	// staticディレクトリは存在しないのでhttp.StripPrefixで取り除く
+	http.Handle("/static/", http.StripPrefix("/static/", files))
+
 	// URLの定義
 	// topの定義はcontrollersのroute_mainで定義している
 	http.HandleFunc("/", top)
--- config/config.go
+++ config/config.go
@@ -12,6 +12,7 @@ type ConfigList struct {
 	SQLDriver string
 	DbName string
 	LogFile string
+	Static string
 }
 
 var Config ConfigList
@@ -32,5 +33,7 @@ func LoadConfig() {
 		SQLDriver: cfg.Section("db").Key("driver").MustString("sqlite3"),
 		DbName: cfg.Section("db").Key("name").String(),
 		LogFile: cfg.Section("web").Key("logfile").String(),
+		// 静的ファイルがありパスをconfig.iniで設定
+		Static: cfg.Section("web").Key("static").String(),
 	}
 }
