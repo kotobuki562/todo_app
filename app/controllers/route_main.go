9506e525773ad75e73778309d668c38558186d60
MaQueen <67810971+kotobuki562@users.noreply.github.com>
2022-07-08 20:33:07 +0900

    📝 データを載せる

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,11 +1,16 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
 	"text/template"
 )
 
+// w,rを受け取るとハンドラーとして定義できる
 func top(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("app/views/templates/top.html")
-	t.Execute(w, nil)
+	t, err := template.ParseFiles("app/views/templates/top.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	t.Execute(w, "Hello")
 }
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -7,6 +7,7 @@ import (
 
 func StartMainServer() error {
 	// URLの定義
+	// topの定義はcontrollersのroute_mainで定義している
 	http.HandleFunc("/", top)
 	// nilにするとデフォルトで404が返ってくる
 	return http.ListenAndServe(":" + config.Config.Port, nil)
