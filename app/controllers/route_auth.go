6ef646b4af3a9e9ae301c45a13a95eb965edf048
MaQueen <67810971+kotobuki562@users.noreply.github.com>
2022-07-19 21:34:47 +0900

    📝 signin登録

--- app/controllers/route_auth.go
+++ app/controllers/route_auth.go
@@ -1,7 +1,34 @@
 package controllers
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"todo_app/app/models"
+)
 
 func signup(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, nil, "layout", "public_navbar", "signup")
+	// GETならテンプレートファイルをレスポンス
+	if r.Method == "GET" {
+		generateHTML(w, nil, "layout", "public_navbar", "signup")
+	} else if r.Method == "POST" {
+		// formで飛んできたデータを解析する
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+		// ユーザーを作成する
+		user := models.User{
+			Name: r.PostFormValue("name"),
+			Email: r.PostFormValue("email"),
+			Password: r.PostFormValue("password"),
+		}
+		// ユーザーを保存する
+		if err := user.CreateUser(); err != nil {
+			log.Println(err)
+		}
+		
+		// 正常に終了した場合はトップページにリダイレクトさせる
+		http.Redirect(w, r, "/", 302)
+	}
+
 }
